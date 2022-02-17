083fa18a149c99561f9ff8b9af8048f8f71dd285
rtaraboo <rtarabocchia@gmail.com>
2022-02-16 20:21:00 -0500

    Work list added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 let items = [];
+let workItems = [];
 
 app.get("/", function(req, res) {
 
@@ -20,27 +21,40 @@ app.get("/", function(req, res) {
   let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items,
   });
 
 });
 
 
-app.post("/", function (req, res){
-  let item = req.body.newItem;
-  items.push(item);
 
-  res.redirect("/");
+
+app.post("/", function (req, res){
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+
+      workItems.push(item);
+      res.redirect("/work");
+
+  } else {
+
+    items.push(item);
+    res.redirect("/");
+
+  }
+
+
 
 });
 
+app.get("/work", function(req, res){
 
+  res.render("list", {listTitle: "Work", newListItems: workItems});
 
-
-
-
-
+});
 
 
 app.listen(3000, function() {
